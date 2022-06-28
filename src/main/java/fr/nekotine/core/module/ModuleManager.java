a82c739e2b9146124caa6d8c06165ba84c004caf
XxGoldenbluexX <tonyg222@outlook.fr>
2022-06-28 22:42:18 +0200

    changement du système de module...encore

--- src/main/java/fr/nekotine/core/module/ModuleManager.java
+++ src/main/java/fr/nekotine/core/module/ModuleManager.java
@@ -5,16 +5,23 @@ import java.util.Map;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.nekotine.core.module.annotation.ModuleNameAnnotation;
+
 public class ModuleManager{
 
-	private Map<String,PluginModule> modules = new HashMap<>();
+	private Map<Class<? extends PluginModule>,PluginModule> modules = new HashMap<>();
+	private Map<String, Class<? extends PluginModule>> nameMappings = new HashMap<>();
 	
 	@SuppressWarnings("unchecked")
 	public void Load(JavaPlugin plugin, Class<? extends PluginModule>... moduleTypes) {
 		for (Class<? extends PluginModule> moduleType : moduleTypes) {
 			try {
 				PluginModule module = moduleType.getConstructor(JavaPlugin.class).newInstance(plugin);
-				modules.put(module.getName(),module);
+				modules.put(moduleType,module);
+				ModuleNameAnnotation annotation = moduleType.getAnnotation(ModuleNameAnnotation.class);
+				if (annotation != null) {
+					nameMappings.put(annotation.Name(), moduleType);
+				}
 			} catch (Exception e) {
 				plugin.getLogger().warning("Impossible de créer le module de type " + moduleType.getTypeName() + '\n' + e.getMessage());
 			}
@@ -22,20 +29,40 @@ public class ModuleManager{
 	}
 	
 	/**
+	 * Récupère un module à partir de sa classe.
+	 * @param <T> Type du module à récuperer.
+	 * @param moduleType Type du module à récuperer.
+	 * @return Le module désiré.
+	 */
+	@SuppressWarnings("unchecked")
+	public <T extends PluginModule> T Get(Class<T> moduleType) {
+		return (T) modules.get(moduleType);
+	}
+	/**
 	 * Récupère le module souhaité par son nom.
 	 * @param name Nom du module.
 	 * @return Le module souhaité.
 	 */
 	public PluginModule Get(String name) {
-		return modules.get(name);
+		Class<? extends PluginModule> clazz = nameMappings.get(name);
+		if (clazz != null) {
+			return modules.get(clazz);
+		}
+		return null;
 	}
 	
+	/**
+	 * Active tous les modules.
+	 */
 	public void enableAll() {
 		for (PluginModule module : modules.values()) {
 			module.enable();
 		}
 	}
 	
+	/**
+	 * Désactive tous les modules.
+	 */
 	public void disableAll() {
 		for (PluginModule module : modules.values()) {
 			module.disable();
