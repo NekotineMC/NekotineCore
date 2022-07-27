e9245c3ed28b4de9b244e6d5a38970600ce035c7
Hy3z <axel.vivenot@outlook.fr>
2022-07-27 20:00:16 +0200

    Ajout d'une fonction pour faire un vortex avec des particules

--- src/main/java/fr/nekotine/core/util/UtilParticle.java
+++ src/main/java/fr/nekotine/core/util/UtilParticle.java
@@ -12,7 +12,7 @@ public class UtilParticle {
 	 * @param particle Particule à afficher
 	 * @param particleNumber Nombre de particules par subdivision
 	 */
-	public static void Circle2D(Location center, float radius, int particleInCircle, Particle particle, int particleNumber) {
+	public static void Circle2D(Location center, double radius, int particleInCircle, Particle particle, int particleNumber) {
 		Circle2D(center, radius, particleInCircle, particle, particleNumber, 0, 0, 0, 0);
 	}
 	/**
@@ -24,7 +24,7 @@ public class UtilParticle {
 	 * @param particleNumber Nombre de particules par subdivision
 	 * @param extra Valeur additionnelle, généralement la vitesse
 	 */
-	public static void Circle2D(Location center, float radius, int particleInCircle, Particle particle, int particleNumber, double extra) {
+	public static void Circle2D(Location center, double radius, int particleInCircle, Particle particle, int particleNumber, double extra) {
 		Circle2D(center, radius, particleInCircle, particle, particleNumber, 0, 0, 0, extra);
 	}
 	/**
@@ -37,13 +37,51 @@ public class UtilParticle {
 	 * @param offsetX Décalage potentiel en x
 	 * @param offsetY Décalage potentiel en y
 	 * @param offsetZ Décalage potentiel en z
+	 * @param extra Valeur additionnelle, généralement la vitesse
 	 */
-	public static void Circle2D(Location center, float radius, int particleInCircle, Particle particle, int particleNumber, double offsetX, double offsetY, double offsetZ, double extra) {
+	public static void Circle2D(Location center, double radius, int particleInCircle, Particle particle, int particleNumber, double offsetX, double offsetY, double offsetZ, double extra) {
 		double range = (2 * Math.PI) / particleInCircle;
-		for(double n = 0 ; n <= 2 * Math.PI ; n+= range) {
+		for(double n = 0 ; n < 2 * Math.PI ; n+= range) {
 			double x = Math.cos(n) * radius;
 			double z = Math.sin(n) * radius;
 			center.getWorld().spawnParticle(particle, center.clone().add(x, 0, z), particleNumber, offsetX, offsetY, offsetZ, extra);
 		}
+	}
+	
+	//
+	
+	/**
+	 * 
+	 * @param bottom Hauteur de départ du vortex
+	 * @param radius Rayon des cercles
+	 * @param particlePerCircle Nombre de subdivisions des cercles
+	 * @param height Hauteur du vortex
+	 * @param circleNumber Nombre de cercles du vortex
+	 * @param particle Particule à afficher
+	 * @param particleNumber Nombre de particules par subdivision
+	 * @param offsetX Décalage potentiel en x
+	 * @param offsetY Décalage potentiel en y
+	 * @param offsetZ Décalage potentiel en z
+	 * @param extra Valeur additionnelle, généralement la vitesse
+	 */
+	public static void Vortex(Location bottom, double radius, int particlePerCircle, double height, int circleNumber, Particle particle, int particleNumber, double offsetX, double offsetY, double offsetZ, double extra) {
+		circleNumber++;
+		double range = (double)height / circleNumber;
+		for(double n = range; n < height ; n+= range) {
+			Circle2D(bottom.clone().add(0, n, 0), radius, particlePerCircle, particle, particleNumber, offsetX, offsetY, offsetZ, extra);
+		}
+	}
+	/**
+	 * 
+	 * @param bottom Hauteur de départ du vortex
+	 * @param radius Rayon des cercles
+	 * @param particlePerCircle Nombre de subdivisions des cercles
+	 * @param height Hauteur du vortex
+	 * @param circleNumber Nombre de cercles du vortex
+	 * @param particle Particule à afficher
+	 * @param particleNumber Nombre de particules par subdivision
+	 */
+	public static void Vortex(Location bottom, double radius, int particlePerCircle, double height, int circleNumber, Particle particle, int particleNumber) {
+		Vortex(bottom, radius, particlePerCircle, height, circleNumber, particle, particleNumber, 0, 0, 0, 0);
 	}
 }
