f436c78a7c24b6ab0be3f4998bd088b6123e73b8
XxGoldenbluexX <tonyg222@outlook.fr>
2022-10-05 20:13:58 +0200

    Fix, retrait de la MainHand dans PlayerInventorySnapshot

--- src/main/java/fr/nekotine/core/inventory/PlayerInventorySnapshot.java
+++ src/main/java/fr/nekotine/core/inventory/PlayerInventorySnapshot.java
@@ -1,6 +1,7 @@
 package fr.nekotine.core.inventory;
 
 import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,23 +13,34 @@ public class PlayerInventorySnapshot extends InventorySnapshot{
 	
 	private Map<EquipmentSlot, ItemStack> equipmentSlots = new HashMap<>();
 	
+	/**
+	 * Pour éviter de sauvegarder la main hand et de la recharger.
+	 */
+	private static EnumSet<EquipmentSlot> savedSlots = EnumSet.of(
+			EquipmentSlot.HEAD,
+			EquipmentSlot.CHEST,
+			EquipmentSlot.LEGS,
+			EquipmentSlot.FEET,
+			EquipmentSlot.OFF_HAND
+			);
+	
 	public void snapshot(PlayerInventory inventory) {
 		content = inventory.getContents();
-		for (EquipmentSlot slot : EquipmentSlot.values()) {
+		for (EquipmentSlot slot : savedSlots) {
 			equipmentSlots.put(slot, inventory.getItem(slot));
 		}
 	}
 	
 	public void deepSnapshot(PlayerInventory inventory) {
 		content = Arrays.asList(inventory.getContents()).stream().map(base -> base.clone()).toArray(ItemStack[]::new);
-		for (EquipmentSlot slot : EquipmentSlot.values()) {
+		for (EquipmentSlot slot : savedSlots) {
 			equipmentSlots.put(slot, inventory.getItem(slot).clone());
 		}
 	}
 	
 	public void fill(PlayerInventory inventory) {
 		inventory.setContents(content);
-		for (EquipmentSlot slot : EquipmentSlot.values()) {
+		for (EquipmentSlot slot : savedSlots) {
 			inventory.setItem(slot, equipmentSlots.get(slot));
 		}
 	}
