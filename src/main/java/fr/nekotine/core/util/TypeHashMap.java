1b0fa51118b4f3493ae98c69420341a3e13a3015
XxGoldenbluexX <tonyg222@outlook.fr>
2023-06-17 14:25:10 +0200

    TypeMap

---
+++ src/main/java/fr/nekotine/core/util/TypeHashMap.java
@@ -0,0 +1,75 @@
+package fr.nekotine.core.util;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class TypeHashMap implements TypeMap{
+
+	Map<Object, Object> backing = new HashMap<>();
+	
+	@Override
+	public int size() {
+		return backing.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return backing.isEmpty();
+	}
+
+	@Override
+	public boolean containsKey(Class<?> key) {
+		return backing.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return backing.containsValue(value);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T remove(Class<T> key) {
+		return (T)backing.remove(key);
+	}
+
+	@Override
+	public void clear() {
+		backing.clear();
+	}
+
+	@Override
+	public Set<Object> keySet() {
+		return backing.keySet();
+	}
+
+	@Override
+	public Collection<Object> values() {
+		return backing.values();
+	}
+
+	@Override
+	public Set<Entry<Object,Object>> entrySet() {
+		return backing.entrySet();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T put(T value) {
+		return (T)backing.put(value.getClass(), value);
+	}
+
+	@Override
+	public <T> T get(Class<T> type) {
+		return type.cast(backing.get(type));
+	}
+
+	@Override
+	public <T> T put(Class<T> type, T value) {
+		return type.cast(backing.put(type, value));
+	}
+
+}
---
+++ src/main/java/fr/nekotine/core/util/TypeMap.java
@@ -0,0 +1,33 @@
+package fr.nekotine.core.util;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.Map.Entry;
+
+public interface TypeMap{
+	
+	public boolean containsKey(Class<?> key);
+	
+	public boolean containsValue(Object key);
+	
+	public int size();
+	
+	public boolean isEmpty();
+	
+	public Set<Object> keySet();
+	
+	public Collection<Object> values();
+	
+	public Set<Entry<Object,Object>> entrySet();
+	
+	public void clear();
+	
+	public <T> T remove(Class<T> key);
+	
+	public <T> T get(Class<T> type);
+	
+	public <T> T put(Class<T> type, T value);
+	
+	public <T> T put(T value);
+	
+}
