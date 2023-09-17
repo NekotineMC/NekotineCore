2acf2b303b4773cf4306f09d4ddd6f14f4832e0b
XxGoldenbluexX <tonyg222@outlook.fr>
2023-09-17 13:02:34 +0200

    mark machine as not running after end

--- src/main/java/fr/nekotine/core/game/phase/PhaseMachine.java
+++ src/main/java/fr/nekotine/core/game/phase/PhaseMachine.java
@@ -102,6 +102,7 @@ public class PhaseMachine implements IPhaseMachine{
 		}
 		var all = getParents(currentPhase);
 		all.add(currentPhase);
+		currentPhase = null;
 		Collections.reverse(all);
 		for (var p : all) {
 			try (var watch = new Stopwatch(w -> LOGGER.log(Level.INFO,"La phase "+p.getClass().getSimpleName()+" est teardown ("+w.elapsedMillis()+" ms)"))){
@@ -111,6 +112,7 @@ public class PhaseMachine implements IPhaseMachine{
 			}
 			runningPhases.remove(p.getClass());
 		}
+		running = false;
 	}
 
 	@Override
