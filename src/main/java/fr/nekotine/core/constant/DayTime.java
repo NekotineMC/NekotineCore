c1cb11fbeb95a0b5d387eac2d2dd15887472ecf6
XxGoldenbluexX <tonyg222@outlook.fr>
2023-09-17 19:59:56 +0200

    Daytime constants

---
+++ src/main/java/fr/nekotine/core/constant/DayTime.java
@@ -0,0 +1,17 @@
+package fr.nekotine.core.constant;
+
+public class DayTime {
+
+	public static final long DAY = 1_000;
+	
+	public static final long NIGHT = 13_000;
+	
+	public static final long NOON = 6_000;
+	
+	public static final long MIDNIGHT = 18_000;
+	
+	public static final long SUNRISE = 23_000;
+	
+	public static final long SUNSET = 12_000;
+	
+}
