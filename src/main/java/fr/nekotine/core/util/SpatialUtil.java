5c4cb4274ef9367bca8c0270347f182afddc2b8f
XxGoldenbluexX <tonyg222@outlook.fr>
2023-10-01 13:53:35 +0200

    3D Line in SpacialUtil

--- src/main/java/fr/nekotine/core/util/SpatialUtil.java
+++ src/main/java/fr/nekotine/core/util/SpatialUtil.java
@@ -1,46 +1,81 @@
 package fr.nekotine.core.util;
 
-import org.bukkit.Location;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 import org.bukkit.util.Vector;
 
 import fr.nekotine.core.util.lambda.TriConsumer;
 
 public class SpatialUtil {
 
-	public static final void circle2DDensity(double centerX, double centerY, double centerZ, double radius, double density, TriConsumer<Double, Double, Double> consumer) {
+	public static final void circle2DDensity(double centerX, double centerY, double radius, double blockDensity, BiConsumer<Double, Double> consumer) {
 		var perimeter = 2 * Math.PI * radius;
-		var delta = perimeter / (density * perimeter);
-		for(double theta = 0 ; theta < perimeter ; theta+=delta) {
+		for(double theta = 0 ; theta < perimeter ; theta+=blockDensity) {
 			double x = centerX + (Math.cos(theta) * radius);
-			double z = centerZ + (Math.sin(theta) * radius);
-			consumer.accept(x, centerY, z);
+			double y = centerY + (Math.sin(theta) * radius);
+			consumer.accept(x, y);
 		}
 	}
 	
-	public static final void circle2DDensity(Vector center, double radius, double density, TriConsumer<Double, Double, Double> consumer) {
-		circle2DDensity(center.getX(), center.getY(), center.getZ(), radius, density, consumer);
-	}
-	
-	public static final void circle2DDensity(Location center, double radius, double density, TriConsumer<Double, Double, Double> consumer) {
-		circle2DDensity(center.getX(), center.getY(), center.getZ(), radius, density, consumer);
-	}
-	
-	public static final void circle2DNumber(double centerX, double centerY, double centerZ, double radius, int points, TriConsumer<Double, Double, Double> consumer) {
+	public static final void circle2DNumber(double centerX, double centerY, double radius, int nbPoints, BiConsumer<Double, Double> consumer) {
 		var fullCircle = 2 * Math.PI;
-		var span = fullCircle / points;
+		var span = fullCircle / nbPoints;
 		for(double theta = 0 ; theta < fullCircle ; theta+=span) {
 			double x = centerX + (Math.cos(theta) * radius);
-			double z = centerZ + (Math.sin(theta) * radius);
-			consumer.accept(x, centerY, z);
+			double y = centerY + (Math.sin(theta) * radius);
+			consumer.accept(x, y);
 		}
 	}
 	
-	public static final void circle2DNumber(Vector center, double radius, int points, TriConsumer<Double, Double, Double> consumer) {
-		circle2DNumber(center.getX(), center.getY(), center.getZ(), radius, points, consumer);
+	public static final void line3DFromDir(Vector start,
+			Vector direction, double distance, double blockDensity,
+			TriConsumer<Double, Double, Double> consumer) {
+		if (!direction.isNormalized()) {
+			direction.normalize();
+		}
+		direction.multiply(blockDensity);
+		for (double theta = 0; theta < distance; theta += blockDensity) {
+			consumer.accept(start.getX(), start.getY(), start.getZ());
+			start.add(direction);
+		}
 	}
 	
-	public static final void circle2DNumber(Location center, double radius, int points, TriConsumer<Double, Double, Double> consumer) {
-		circle2DNumber(center.getX(), center.getY(), center.getZ(), radius, points, consumer);
+	public static final void line3DFromDir(double startX, double startY, double startZ,
+			Vector direction, double distance, double blockDensity,
+			TriConsumer<Double, Double, Double> consumer) {
+		line3DFromDir(new Vector(startX, startY, startZ), direction, distance, blockDensity, consumer);
+	}
+	
+	public static final void line3DFromDir(double startX, double startY, double startZ,
+			double dirx, double diry, double dirz, double distance, double blockDensity,
+			TriConsumer<Double, Double, Double> consumer) {
+		line3DFromDir(new Vector(startX, startY, startZ), new Vector(dirx, diry, dirz), distance, blockDensity, consumer);
+	}
+	
+	public static final void line3DFromDir(Vector start,
+			Vector direction, double distance, double blockDensity,
+			Consumer<Vector> consumer) {
+		if (!direction.isNormalized()) {
+			direction.normalize();
+		}
+		direction.multiply(blockDensity);
+		for (double theta = 0; theta < distance; theta += blockDensity) {
+			consumer.accept(start);
+			start.add(direction);
+		}
+	}
+	
+	public static final void line3DFromDir(double startX, double startY, double startZ,
+			Vector direction, double distance, double blockDensity,
+			Consumer<Vector> consumer) {
+		line3DFromDir(new Vector(startX, startY, startZ), direction, distance, blockDensity, consumer);
+	}
+	
+	public static final void line3DFromDir(double startX, double startY, double startZ,
+			double dirx, double diry, double dirz, double distance, double blockDensity,
+			Consumer<Vector> consumer) {
+		line3DFromDir(new Vector(startX, startY, startZ), new Vector(dirx, diry, dirz), distance, blockDensity, consumer);
 	}
 	
 }
