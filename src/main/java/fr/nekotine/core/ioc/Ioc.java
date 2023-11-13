f3ab03a283f0495a713d94573d4f460a95d973a3
XxGoldenbluexX <tonyg222@outlook.fr>
2023-11-13 20:01:59 +0100

    doc Ioc

--- src/main/java/fr/nekotine/core/ioc/Ioc.java
+++ src/main/java/fr/nekotine/core/ioc/Ioc.java
@@ -2,16 +2,31 @@ package fr.nekotine.core.ioc;
 
 public class Ioc {
 
-	private static IIocProvider provider;
+	private static IIocProvider provider = new IocProvider();
 	
+	/**
+	 * Replace default IocProvider
+	 * @param provider
+	 */
 	public static void setProvider(IIocProvider provider) {
 		Ioc.provider = provider;
 	}
 	
+	/**
+	 * Get configured IocProvider
+	 * @return
+	 */
 	public static IIocProvider getProvider() {
 		return Ioc.provider;
 	}
 	
+	/**
+	 * Resolve dependency using configured IocProvider.
+	 * If none are set, a default one will be used
+	 * @param <T>
+	 * @param type
+	 * @return
+	 */
 	public static <T> T resolve(Class<T> type) {
 		return provider.resolve(type);
 	}
