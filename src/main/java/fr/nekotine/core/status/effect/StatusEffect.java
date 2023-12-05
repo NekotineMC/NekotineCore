80fe190f2f9ff94292877d44f205579c7f014055
XxGoldenbluexX <tonyg222@outlook.fr>
2023-12-05 20:39:00 +0100

    petite aide pour créer un status effect

--- src/main/java/fr/nekotine/core/status/effect/StatusEffect.java
+++ src/main/java/fr/nekotine/core/status/effect/StatusEffect.java
@@ -1,4 +1,12 @@
 package fr.nekotine.core.status.effect;
 
+/**
+ * 
+ * Duration = durée de l'effet en tick, -1 pour une durée infinie
+ * 
+ * @author XxGoldenbluexX
+ *
+ *
+ */
 public record StatusEffect(StatusEffectType type, int duration) {
 }
