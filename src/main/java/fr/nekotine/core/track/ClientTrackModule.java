f34e80f6de201c063db8716a0a7959487fa132a9
XxGoldenbluexX <tonyg222@outlook.fr>
2023-12-23 11:21:05 +0100

    untrack held item change

--- src/main/java/fr/nekotine/core/track/ClientTrackModule.java
+++ src/main/java/fr/nekotine/core/track/ClientTrackModule.java
@@ -9,23 +9,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.events.PacketListener;
 
 import fr.nekotine.core.ioc.Ioc;
 import fr.nekotine.core.module.PluginModule;
 
-public class ClientMovementTrackModule extends PluginModule{
+public class ClientTrackModule extends PluginModule{
 	private Collection<Player> map = new LinkedList<>();
 	private PacketListener packetAdapter = new PacketAdapter(Ioc.resolve(JavaPlugin.class),
 			PacketType.Play.Client.POSITION,
 			PacketType.Play.Client.POSITION_LOOK,
 			PacketType.Play.Client.LOOK,
-			PacketType.Play.Client.GROUND) {
+			PacketType.Play.Client.GROUND,
+			PacketType.Play.Client.HELD_ITEM_SLOT) {
 		
 		public void onPacketReceiving(PacketEvent event) {
-			PacketContainer packet = event.getPacket();
 			Player player = event.getPlayer();
 			if (map.contains(player)) {
 				event.setCancelled(true);
@@ -33,7 +32,7 @@ public class ClientMovementTrackModule extends PluginModule{
 		}
 	};
 	
-	public ClientMovementTrackModule() {
+	public ClientTrackModule() {
 		var pmanager = ProtocolLibrary.getProtocolManager();
 		pmanager.addPacketListener(packetAdapter);
 	}
