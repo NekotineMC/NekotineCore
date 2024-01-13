fce800cf50c348fc5ab345fb546640c46edf0cd5
XxGoldenbluexX <tonyg222@outlook.fr>
2024-01-13 13:34:49 +0100

    Automatic removal of DebugUtil created entitites

--- src/main/java/fr/nekotine/core/NekotinePlugin.java
+++ src/main/java/fr/nekotine/core/NekotinePlugin.java
@@ -2,10 +2,12 @@ package fr.nekotine.core;
 
 import fr.nekotine.core.ioc.Ioc;
 import fr.nekotine.core.module.ModuleManager;
+import fr.nekotine.core.util.DebugUtil;
 
 public class NekotinePlugin {
 
 	public void disable() {
+		DebugUtil.clearDebugInstances();
 		Ioc.resolve(ModuleManager.class).unloadAll();
 	}
 	
--- src/main/java/fr/nekotine/core/util/DebugUtil.java
+++ src/main/java/fr/nekotine/core/util/DebugUtil.java
@@ -1,5 +1,8 @@
 package fr.nekotine.core.util;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 import org.bukkit.World;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.BlockDisplay;
@@ -11,6 +14,17 @@ import org.joml.Vector3f;
 
 public class DebugUtil {
 
+	private static DebugUtil instance = new DebugUtil();
+	
+	private Collection<BlockDisplay> debugEntities = new LinkedList<>();
+	
+	private void clear() {
+		for (var e : debugEntities) {
+			e.remove();
+		}
+		debugEntities.clear();
+	}
+	
 	public static BlockDisplay debugBoundingBox(World world, BoundingBox box, BlockData data) {
 		var min = box.getMin();
 		var max = box.getMax();
@@ -19,7 +33,12 @@ public class DebugUtil {
 		var display = (BlockDisplay)world.spawnEntity(min.toLocation(world), EntityType.BLOCK_DISPLAY);
 		display.setBlock(data);
 		display.setTransformation(transform);
+		instance.debugEntities.add(display);
 		return display;
+	}
+	
+	public static void clearDebugInstances() {
+		instance.clear();
 	}
 	
 }
