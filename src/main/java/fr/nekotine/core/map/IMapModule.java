69c47ab63809b98bc7c7ae26241147ff5cd07a25
XxGoldenbluexX <tonyg222@outlook.fr>
2024-02-06 20:15:12 +0100

    new map module specifications

---
+++ src/main/java/fr/nekotine/core/map/IMapModule.java
@@ -0,0 +1,68 @@
+package fr.nekotine.core.map;
+
+import java.util.Collection;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
+
+/**
+ * Module synchrone pour récupérer charger et sauvegarder des cartes et leurs composants
+ * 
+ * @author XxGoldenbluexX
+ * 
+ */
+public interface IMapModule {
+
+	/**
+	 * Retourne la liste des maps présentes sur le serveur.
+	 * @return Une collection avec la metadata des cartes.
+	 */
+	public @Nonnull Collection<MapMetadata> listMaps();
+	
+	/**
+	 * Obtient la metadata de la carte si elle est sur le serveur.
+	 * @param name Le nom de la carte à trouver.
+	 * @return La matedata de la carte, ou null si elle n'est pas sur le serveur.
+	 */
+	public @Nullable MapMetadata getMapMetadata(@Nonnull String name);
+	
+	/**
+	 * Sauvegarde la metadata de la carte si elle existe.
+	 * @param metadata La metadata de la carte
+	 */
+	public void saveMapMetadata(@Nonnull MapMetadata metadata);
+	
+	/**
+	 * Obtient la structure de la carte si elle existe.
+	 * @param metadata La metadata de la carte.
+	 * @return La structure de la carte si elle existe.
+	 */
+	public @Nullable Clipboard getStructure(@Nonnull MapMetadata metadata);
+	
+	/**
+	 * Sauvegarde la structure de la carte si elle existe.
+	 * @param metadata La metadata de la carte.
+	 * @param structure La structure de la carte.
+	 */
+	public void saveStructure(@Nonnull MapMetadata metadata, Clipboard structure);
+	
+	
+	/**
+	 * Obtient le contenu de la carte si elle existe.
+	 * @param <T> Le type du contenu de la carte.
+	 * @param metadata La metadata de la carte.
+	 * @param mapType Le type du contenu de la carte.
+	 * @return Le contenu de la carte si elle existe.
+	 */
+	public <T> @Nullable T getContent(@Nonnull MapMetadata metadata, @Nonnull Class<T> mapType);
+	
+	/**
+	 * Sauvegarde le contenu de la carte si elle existe.
+	 * @param <T> Le type du contenu de la carte.
+	 * @param metadata La metadata de la carte.
+	 */
+	public <T> void saveContent(@Nonnull MapMetadata metadata, T content);
+	
+}
